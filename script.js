7afbe5ca63811fade78ae3c34f295f61c834e80c
Aryan <aryandineshsingh1@gmail.com>
2026-09-07 12:09:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,44 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+function downloadImage(url) {
+  return new Promise((resolve, reject) => {
+    const img = document.createElement("img");
+
+    img.onload = function () {
+      resolve(url);
+    };
+
+    img.onerror = function () {
+      reject(`Failed to download image: ${url}`);
+    };
+
+    img.src = url;
+  });
+}
+
+function downloadImages() {
+  loading.textContent = "Loading...";
+  errorDiv.textContent = "";
+  output.innerHTML = "";
+
+  const promises = images.map((image) => downloadImage(image.url));
+
+  Promise.all(promises)
+    .then((urls) => {
+      loading.textContent = "";
+
+      urls.forEach((url) => {
+        const img = document.createElement("img");
+        img.src = url;
+        output.appendChild(img);
+      });
+    })
+    .catch((error) => {
+      loading.textContent = "";
+      errorDiv.textContent = error;
+    });
+}
+
+downloadImages();
